31b7cfdb79d988882b25c72b8265faea524b65bf
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-05-18 08:59:59 +0900

    Fix

--- test/test_con12.bash
+++ test/test_con12.bash
@@ -3,6 +3,6 @@
 com=$(basename $0 | sed 's/test_//;s/\.bash//')
 com=$(dirname $0)/../bin/$com
 
-output=$( $com ls hofehaoeoh /etc/passwd)
+output=$( $com ls hofehaoeoh /etc/passwd || true)
 
 [ "$(wc -l <<< $output)" = "2" ]
