8e5a0ac30ba33cc34dc7d606a24043c9df6562ad
Ryuichi Ueda <ryuichiueda@gmail.com>
2021-05-18 08:53:05 +0900

    Add dummy test

--- test/test_con12.bash
+++ test/test_con12.bash
@@ -3,6 +3,6 @@
 com=$(basename $0 | sed 's/test_//;s/\.bash//')
 com=$(dirname $0)/../bin/$com
 
-output=$( $com ls hofehaoeoh /etc/passwd | wc -l)
+output=$( $com ls hofehaoeoh /etc/passwd)
 
 [ "$output" = "2" ]
